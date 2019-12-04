3719c5ef63c364cd9edb396b90ae1af651943892
Emma Vanbrabant <emmag87@gmail.com>
2019-12-04 20:26:43 +0000

    Day 4 II

--- day4.kt
+++ day4.kt
@@ -4,7 +4,7 @@ var max = 787419
 fun main() {
     val validNumbers = mutableListOf<Int>()
     for (x in min..max) {
-        print("checking: $x\n")
+        // print("checking: $x\n")
         if (isValid(x)) {
             validNumbers.add(x)
         }
@@ -14,31 +14,26 @@ fun main() {
 
 fun isValid(number: Int): Boolean {
     var currentNumber = number
-    var lastDigit = -1
-    var hasTwoAdjacentDigits = false
-    // Two adjacent digits are the same (like 22 in 122345).
+    val digitFrequencyMap = mutableMapOf<Int, Int>()
 
     while (true) {
         val currentDigit = currentNumber % 10
         val nextDigit = (currentNumber / 10) % 10
-        // print("digit: $currentDigit, next: $nextDigit\n")
+        if (!digitFrequencyMap.contains(currentDigit)) {
+            digitFrequencyMap.put(currentDigit, 1)
+        } else {
+            digitFrequencyMap.put(currentDigit, digitFrequencyMap.get(currentDigit)!! + 1)
+        }
 
         // Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-        if(nextDigit > currentDigit) {
-            // print("next too large, returning digit: $currentDigit, next: $nextDigit\n")
+        if (nextDigit > currentDigit) {
             return false
         }
 
-        if(lastDigit != -1 && lastDigit == currentDigit) {
-            // print("has two adjacent digits: $lastDigit\n")
-            hasTwoAdjacentDigits = true
-        }
-        
-        lastDigit = currentDigit
         currentNumber /= 10
         if (currentNumber <= 0) {
             break
         }
     }
-    return hasTwoAdjacentDigits
+    return digitFrequencyMap.values.filter { it == 2 }.count() >= 1
 }
