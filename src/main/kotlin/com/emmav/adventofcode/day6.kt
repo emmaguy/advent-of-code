67474e275afed8a2ce6574a3027dd10a8d10089b
Emma Guy <emmag87@gmail.com>
2019-12-08 13:51:03 +0000

    Day 6 Part II

--- src/main/kotlin/com/emmav/adventofcode/day6.kt
+++ src/main/kotlin/com/emmav/adventofcode/day6.kt
@@ -3,7 +3,7 @@ import java.io.File
 var total = 0
 
 fun main() {
-    val input = File("input/day6input.txt").readLines()
+    val input = File("input/day6.txt").readLines()
 
     val map = mutableMapOf<String, Object>()
 
@@ -15,6 +15,44 @@ fun main() {
         objectBeingOrbited.orbitedBy.add(objectDirectlyOrbiting)
     }
 
+    map.forEach { (_, parent) ->
+        parent.orbitedBy.forEach { it.parent = parent }
+    }
+
+    // find all parents of both
+    // find the first parent which is common
+    val parentsOfYou = allParentsOf(map["YOU"]!!.parent!!)
+    val parentsOfSanta = allParentsOf(map["SAN"]!!.parent!!)
+
+    var total = 0
+    for (parentOfYou in parentsOfYou) {
+        val find = parentsOfSanta.find { it.first == parentOfYou.first }
+        if (find != null) {
+            total = parentOfYou.second + find.second
+            break
+        }
+    }
+
+    println("$total")
+
+}
+
+fun allParentsOf(objectSantaIsOrbiting: Object): List<Pair<Object, Int>> {
+    val list = mutableListOf<Pair<Object, Int>>()
+    var currentObject = objectSantaIsOrbiting
+    var index = 0
+    while (true) {
+        list.add(Pair(currentObject, index++))
+        if (currentObject.parent != null) {
+            currentObject = currentObject.parent!!
+        } else {
+            break
+        }
+    }
+    return list
+}
+
+private fun part1(map: MutableMap<String, Object>) {
     val root = map["COM"]!!
     visit(root, 0)
     print("total: $total\n")
@@ -33,5 +71,6 @@ fun visit(obj: Object, level: Int): Int {
 }
 
 data class Object(val name: String) {
+    var parent: Object? = null
     val orbitedBy = mutableListOf<Object>()
 }
