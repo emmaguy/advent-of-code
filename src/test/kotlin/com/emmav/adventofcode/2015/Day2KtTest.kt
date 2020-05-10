2e3eed02159857435773e1e9881df835433bb9b3
Emma Vanbrabant <emmag87@gmail.com>
2020-05-10 15:48:12 +0100

    Day 2

---
+++ src/test/kotlin/com/emmav/adventofcode/2015/Day2KtTest.kt
@@ -0,0 +1,23 @@
+package com.emmav.adventofcode.`2015`
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class Day2KtTest {
+
+    @Test fun `example1`() {
+        assertEquals(58, wrappingPaperRequired("2x3x4"))
+    }
+
+    @Test fun `example2`() {
+        assertEquals(43, wrappingPaperRequired("1x1x10"))
+    }
+
+    @Test fun `example1 part2`() {
+        assertEquals(34, ribbonRequired("2x3x4"))
+    }
+
+    @Test fun `example2 part2`() {
+        assertEquals(14, ribbonRequired("1x1x10"))
+    }
+}
